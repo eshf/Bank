69b351a9906a1cce3b13e578e5687e5142209962
eshf <62289445+eshf@users.noreply.github.com>
2022-12-13 02:26:56 +0800

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,44 @@
+
+import java.util.LinkedHashMap;
+
+/**
+ * Private Variables:<br>
+ * {@link #accounts}: List&lt;Long, Account&gt;
+ */
+public class Bank implements BankInterface {
+    public Bank() {
+        // complete the function
+        this.Bank();
+    }
+
+    private Account getAccount(Long accountNumber) {
+        // complete the function
+        getAccount(accountNumber);
+        return accountNumber;
+    }
+
+    public boolean authenticateUser(Long accountNumber, int pin) {
+        // complete the function
+        authenticateUser(accountNumber, pin);
+        return true;
+    }
+
+    public double getBalance(Long accountNumber) {
+        // complete the function
+        while (getBalance(accountNumber == 1)) {
+            return accountNumber;
+        }
+        return -1;
+    }
+
+    public void credit(Long accountNumber, double amount) {
+        // complete the function
+        credit.getAccount();
+    }
+
+    public boolean debit(Long accountNumber, double amount) {
+        // complete the function
+        debit.getBalance();
+        return true;
+    }
+}
