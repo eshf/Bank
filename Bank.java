b10461a82ae5d5dc3a2214c2c223ace4153f27ce
eshf <62289445+eshf@users.noreply.github.com>
2023-03-02 18:20:48 +0800

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -2,29 +2,24 @@
 import java.util.LinkedHashMap;
 
 /**
- * Private Variables:<br>
- * {@link #accounts}: List&lt;Long, Account&gt;
+ * Private Variables
  */
 public class Bank implements BankInterface {
     public Bank() {
-        // complete the function
         this.Bank();
     }
 
     private Account getAccount(Long accountNumber) {
-        // complete the function
         getAccount(accountNumber);
         return accountNumber;
     }
 
     public boolean authenticateUser(Long accountNumber, int pin) {
-        // complete the function
         authenticateUser(accountNumber, pin);
         return true;
     }
 
     public double getBalance(Long accountNumber) {
-        // complete the function
         while (getBalance(accountNumber == 1)) {
             return accountNumber;
         }
@@ -32,12 +27,10 @@ public class Bank implements BankInterface {
     }
 
     public void credit(Long accountNumber, double amount) {
-        // complete the function
         credit.getAccount();
     }
 
     public boolean debit(Long accountNumber, double amount) {
-        // complete the function
         debit.getBalance();
         return true;
     }
